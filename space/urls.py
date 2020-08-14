5c4396ad6ac88f4df653e25bb0b4020ecf0f2158
Greece4ever <66337535+Greece4ever@users.noreply.github.com>
2020-08-15 00:22:25 +0300

    Removed stupid comment

--- space/urls.py
+++ space/urls.py
@@ -4,7 +4,7 @@ from django.views.generic import TemplateView
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls), #Not suitable for production
+    path('admin/', admin.site.urls),
     path('apiconfig/',include('api.urls')),
     path('',TemplateView.as_view(template_name="index.html")),
     re_path(r'^(?:.*)/?$',TemplateView.as_view(template_name="index.html"))
